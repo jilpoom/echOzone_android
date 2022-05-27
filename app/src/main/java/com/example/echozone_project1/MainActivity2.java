91bbf9929f5b4964f9204d965d826b2fabd41b13
CJW <qscft7000@gmail.com>
2022-05-27 16:53:40 +0900

    QR코드 구현

---
+++ app/src/main/java/com/example/echozone_project1/MainActivity2.java
@@ -0,0 +1,29 @@
+package com.example.echozone_project1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity2 extends AppCompatActivity {
+    private Button createQRBTn;
+    private Button scanQRBtn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+    createQRBTn = (Button) findViewById(R.id.createQR);
+
+    createQRBTn.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            Intent intent = new Intent(MainActivity2.this, CreateQR.class);
+            startActivity(intent);
+        }
+    });
+
+    }
+}
