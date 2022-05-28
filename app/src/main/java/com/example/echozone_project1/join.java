20cbdaa72abdb0081650138f1aa1150c08a3ad14
smhrd <nmmng03@gmail.com>
2022-05-28 17:15:17 +0900

    현지 - 로그인/회원가입 페이지

---
+++ app/src/main/java/com/example/echozone_project1/join.java
@@ -0,0 +1,14 @@
+package com.example.echozone_project1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class join extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_join);
+    }
+}
